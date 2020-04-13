6f63c33ad7e3586424ea882fe6614623e37f8b57
XfHua <jasonHuaxf@163.com>
2020-04-13 15:13:18 +0800

    Add files via upload

---
+++ App204/App204/App204.Android/HybridWebViewRenderer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+using App204.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using WebView = Xamarin.Forms.WebView;
+
+[assembly: ExportRenderer(typeof(WebView), typeof(HybridWebViewRenderer))]
+namespace App204.Droid
+{
+    public class HybridWebViewRenderer : WebViewRenderer
+    {
+        public HybridWebViewRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<WebView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null)
+            {
+
+            }
+            if (e.NewElement != null)
+            {
+                //Control.SetWebChromeClient(new FullScreenClient(linearLayout, contentLayout));
+            }
+        }
+    }
+
+    public class FullScreenClient : WebChromeClient
+    {
+        readonly FrameLayout.LayoutParams matchParentLayout = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent,
+                                                                                                         ViewGroup.LayoutParams.MatchParent);
+        readonly ViewGroup content;
+        readonly ViewGroup parent;
+        Android.Views.View customView;
+
+        public FullScreenClient(ViewGroup parent, ViewGroup content)
+        {
+            this.parent = parent;
+            this.content = content;
+        }
+
+        public override void OnShowCustomView(Android.Views.View view, ICustomViewCallback callback)
+        {
+            customView = view;
+            view.LayoutParameters = matchParentLayout;
+            parent.AddView(view);
+            content.Visibility = ViewStates.Gone;
+        }
+
+        public override void OnHideCustomView()
+        {
+            content.Visibility = ViewStates.Visible;
+            parent.RemoveView(customView);
+            customView = null;
+        }
+    }
+}
---
+++ App204/App204/App204/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App204
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void OnButtonClicked(object sender, EventArgs e)
+        {
+            var status = await CrossPermissions.Current.CheckPermissionStatusAsync<StoragePermission>();
+            if (status != PermissionStatus.Granted)
+            {
+                // The error occurs here 
+                if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.Storage))
+                {
+                    await DisplayAlert("Access needed", "This application uses storage", "OK");
+                }
+
+                status = await CrossPermissions.Current.RequestPermissionAsync<StoragePermission>();
+            }
+
+            if (status == PermissionStatus.Granted)
+            {
+                // my logic
+            }
+            else
+            {
+                await DisplayAlert("Storage access denied", "Can not continue, try again.", "OK");
+            }
+        }
+    }
+}
